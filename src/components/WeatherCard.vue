782783f745202e8031ce91dd18fb85447ddc8ef8
Karalyos Béla <karalyosbela@gmail.com>
2021-05-17 12:29:37 +0200

    kov 7 nap lekeredezese

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -7,10 +7,6 @@
       v-model="value"
       @change="getData(value)"
     ></v-autocomplete>
-
-    <div v-for="(item, index) in cityData" :key="index">
-      {{ item }}
-    </div>
   </div>
 </template>
 
@@ -25,16 +21,6 @@ export default {
       value: "",
       data: "",
       cities: [],
-      cityData: {
-        weather: "",
-        weatherDesc: "",
-        feelsLike: "",
-        temperature: "",
-        humidity: "",
-        clouds: "",
-        windspeed: "",
-        windDirection: "",
-      },
     };
   },
   methods: {
@@ -47,31 +33,16 @@ export default {
             "+&appid=b203fc1026c241d3e13b9713a3665286&lang=hu"
         )
         .then((response) => {
-          // console.log(JSON.stringify(response.data));
-          var latAndLon = response.data.coord;
-          this.data = response.data;
-          console.log(latAndLon);
-          // this.cityData.temperature = response.data.main.temp;
-          // this.cityData.feelsLike = response.data.main.feels_like;
-          // this.cityData.humidity = response.data.main.humidity;
-          // this.cityData.windspeed = response.data.wind.speed;
-          // this.cityData.windDirection = response.data.wind.deg;
-          // this.cityData.weather = response.data.weather[0].main;
-          // this.cityData.weatherDesc = response.data.weather[0].description;
-          // this.cityData.clouds = response.data.clouds.all;
-          // console.log(response.data.weather);
-        })
-        .then((latAndLon) => {
           axios
             .get(
               "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-                latAndLon.lat +
+                response.data.coord.lat +
                 "&lon=" +
-                latAndLon.lon +
-                "&exclude=daily&appid=b203fc1026c241d3e13b9713a3665286"
+                response.data.coord.lon +
+                "&units=metric&appid=b203fc1026c241d3e13b9713a3665286"
             )
             .then((response) => {
-              console.log("asdA" + response);
+              console.log(response.data);
             });
         })
         .catch((err) => {
