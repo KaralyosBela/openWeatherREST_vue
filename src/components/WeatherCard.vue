91546823cc3381c84e5a695baeaf85a392de6151
Karalyos Béla <karalyosbela@gmail.com>
2021-06-09 14:58:54 +0200

    hetiCard done

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -13,12 +13,13 @@
         {{ errorMessage }}
       </v-alert>
     </v-col>
-    <!-- <v-row align="center" justify="space-around" class="pb-2">
+
+    <v-row align="center" justify="space-around" class="pb-2" >
       <v-col><v-btn block rounded color="primary">Hourly</v-btn></v-col>
       <v-col><v-btn block rounded color="primary">Daily</v-btn></v-col>
       <v-col><v-btn block rounded color="primary">Weekly</v-btn></v-col>
-    </v-row> -->
-
+    </v-row>
+     
     <v-card
       rounded="lg"
       class="mx-auto ma-2 pa-2"
@@ -50,7 +51,7 @@
             lazy-src="https://picsum.photos/id/11/10/6"
             max-height="100"
             max-width="100"
-            :src="weatherIcon"
+            :src="weatherIconForCurrent"
           >
           </v-img
         ></v-col>
@@ -92,6 +93,104 @@
           {{ moment.unix(currentWeather.sunset).format("LTS") }}
         </v-col>
       </v-row>
+    </v-card> 
+
+    <v-card
+      rounded="lg"
+      class="mx-auto ma-2 pa-2"
+      max-width="600"
+      v-if="this.$store.state.loaded && alerts"
+    >
+      <v-expansion-panels multiple>
+        <v-expansion-panel v-for="(item, index) in alerts" :key="index">
+          <v-expansion-panel-header class="text-h6">
+            {{ item.event }}
+            <template v-slot:actions>
+              <v-icon color="warning"> mdi-alert-circle </v-icon>
+            </template>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            ettől: {{ moment.unix(item.start).format("lll") }} eddig:
+            {{ moment.unix(item.end).format("lll") }}
+            {{ item.description }}
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-card>
+
+    <v-card
+      rounded="lg"
+      class="mx-auto ma-2 pa-2"
+      max-width="600"
+      v-if="this.$store.state.loaded"
+    >
+      <v-row align="center" justify="center">
+        <v-col cols="9">
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title class="text-h3">
+                {{ cityName }}
+              </v-list-item-title>
+              <v-list-item-subtitle class="text-h6">
+                {{
+                  currentTime.time +
+                  " " +
+                  currentTime.day +
+                  ", " +
+                  dailyWeather[pickedDay].weather[0].description
+                }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item></v-col
+        >
+        <v-col cols="3" class="d-flex justify-center">
+          <v-img
+            id="img"
+            lazy-src="https://picsum.photos/id/11/10/6"
+            max-height="100"
+            max-width="100"
+            :src="weatherIconForDaily"
+          >
+          </v-img
+        ></v-col>
+      </v-row>
+
+      <v-card-text>
+        <v-row align="center">
+          <v-col class="text-h4" cols="12" md="6" sm="12">
+            hőmérséklet <br />
+            {{ dailyWeather[pickedDay].temp.day }} &deg;C
+          </v-col>
+
+          <v-col class="text-h4" cols="12" md="6" sm="12">
+            hőérzet <br />
+            {{ dailyWeather[pickedDay].feels_like.day }} &deg;C
+          </v-col>
+        </v-row>
+      </v-card-text>
+
+      <v-row
+        align="center"
+        justify="space-around"
+        class="text-center pa-1 ma-1"
+      >
+        <v-col>
+          <v-icon class="mb-1">mdi-windsock</v-icon>
+          {{ dailyWeather[pickedDay].wind_speed }} km/h
+        </v-col>
+        <v-col>
+          <v-icon class="mb-1">mdi-water-percent</v-icon>
+          {{ dailyWeather[pickedDay].humidity }} %
+        </v-col>
+        <v-col>
+          <v-icon class="mb-1">mdi-weather-sunset-up</v-icon>
+          {{ moment.unix(dailyWeather[pickedDay].sunrise).format("LTS") }}
+        </v-col>
+        <v-col>
+          <v-icon class="mb-1">mdi-weather-sunset-down</v-icon>
+          {{ moment.unix(dailyWeather[pickedDay].sunset).format("LTS") }}
+        </v-col>
+      </v-row>
 
       <v-slider
         v-model="pickedDay"
@@ -102,31 +201,6 @@
         track-fill-color="blue"
         @change="changePickedDay()"
       ></v-slider>
-    </v-card>
-
-    <v-card
-      rounded="lg"
-      class="mx-auto ma-2 pa-2"
-      max-width="600"
-      v-if="this.$store.state.loaded && alerts"
-    >
-      <v-expansion-panels>
-        <v-expansion-panel v-for="(item, index) in alerts" :key="index">
-          <v-expansion-panel-header class="text-h6">
-            {{ item.event }} 
-            <template v-slot:actions>
-            <v-icon color="warning">
-              mdi-alert-circle
-            </v-icon>
-          </template>
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            ettől: {{ moment.unix(item.start).format("lll") }}
-            eddig: {{ moment.unix(item.end).format("lll") }}
-            {{ item.description }}
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
     </v-card>
   </div>
 </template>
@@ -145,7 +219,8 @@ export default {
       data: "",
       cities: [],
       alerts: [],
-      weatherIcon: "",
+      weatherIconForCurrent: "",
+      weatherIconForDaily: "",
       currentTime: {
         interval: "",
         time: "",
@@ -168,19 +243,25 @@ export default {
           //kellett mert az első város kiválasztásánál még nem töltötte be a jsont a vuexbe
           console.log(this.currentWeather);
           this.alerts = this.$store.state.weatherAlerts;
-          this.weatherIcon =
+          this.weatherIconForCurrent = this.weatherIconForDaily =
             "http://openweathermap.org/img/wn/" +
             this.currentWeather.weather[0].icon +
             ".png";
         }, 500);
       }
     },
-    // changePickedDay(){
-
-    // }
+    changePickedDay() {
+      this.weatherIconForDaily =
+        "http://openweathermap.org/img/wn/" +
+        this.dailyWeather[this.pickedDay].weather[0].icon +
+        ".png";
+    },
   },
 
   computed: {
+    dailyWeather() {
+      return this.$store.state.dailyWeatherData;
+    },
     currentWeather() {
       //var x = this.pickedDay;
       return this.$store.state.currentWeatherData; //currentWeatherData
@@ -202,7 +283,7 @@ export default {
     },
   },
   created() {
-    moment.locale('hu');
+    moment.locale("hu");
     //Magyar városok kiválasztása
     this.cities = cities.filter((x) => x.country == "HU");
 
--- src/store/index.js
+++ src/store/index.js
@@ -45,7 +45,6 @@ export default new Vuex.Store({
             "+&appid=b203fc1026c241d3e13b9713a3665286&lang=hu"
         )
         .then((response) => {
-          //console.log("elso axios megtortent");
           axios
             .get(
               "https://api.openweathermap.org/data/2.5/onecall?lat=" +
@@ -55,9 +54,6 @@ export default new Vuex.Store({
                 "&lang=hu&units=metric&appid=b203fc1026c241d3e13b9713a3665286"
             )
             .then((response) => {
-              //console.log("masodik axios megtortent");
-              //console.log(response.data);
-              //commit("setWeatherData",response.data);
               commit("setWeatherData", {
                 responseData: response.data,
                 cityName: payload.cityNameOriginal
